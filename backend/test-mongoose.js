fbf255a70297f17313badc14f0d77cdb07af8602
Dhanush <dhanush@siriartsandcrafts.com>
2026-06-05 22:59:44 +0530

    chore: remove unnecessary temporary files from git tracking and add to gitignore

--- backend/test-mongoose.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-async function run() {
-  await mongoose.connect('mongodb://localhost:27017/test');
-  const Model = mongoose.model('Test', new mongoose.Schema({ name: String }));
-  await Model.findOneAndUpdate({}, { name: 'test' }, { new: true });
-  console.log('Done');
-  process.exit(0);
-}
-run();
